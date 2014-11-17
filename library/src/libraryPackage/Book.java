028a2b5c12272dc28ea9cb559131dc1ec731b773
Nathan Baum <nbaum510@gmail.com>
2014-11-17 13:56:52 -0800

    Library Creation
    created library project, package, and class for book, and library driver

---
+++ library/src/libraryPackage/Book.java
@@ -0,0 +1,26 @@
+package libraryPackage;
+
+public class Book {
+	private String title, author;
+	
+	public Book(String title, String author){
+		this.title = title;
+		this.author = author;
+	}
+	
+	public String getTitle(){
+		return title;
+	}
+	
+	public String getAuthor(){
+		return author;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+}
