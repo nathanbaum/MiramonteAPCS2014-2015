a01bc32a9ee3bae9f23c30eda194dfeb1ff7b8a1
Nathan Baum <nbaum510@gmail.com>
2014-12-04 14:10:49 -0800

    BinarySearch Start

---
+++ Homework/src/HomeworkPackage/BinarySearch.java
@@ -0,0 +1,14 @@
+package HomeworkPackage;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private int range, 
+	
+	public 
+
+}
