1967ddd571dbf7785e5883f56dd1d2ad654e444e
3004728 <3004728@MM-444-13.mhs.acalanes.k12.ca.us>
2015-02-06 14:12:31 -0800

    changes

--- ConnectFour/src/connectFourPackage/Driver.java
+++ ConnectFour/src/connectFourPackage/Driver.java
@@ -5,20 +5,25 @@ public class Driver {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		GameManager game = new GameManager(scan);
-		int counter = 0;
-		
-		while(!game.isWin){
-			if(counter==0){
-				game.turn(1);
-				counter++;
+		while(true){
+			GameManager game = new GameManager(scan);
+			int counter = 0;
+			String uIn;
+			while(!game.isWin){
+				uIn = "";
+				System.out.println("Guns! Fireworks! Football! Women! Men! And Everything in between! Cars! Alcohol! Node.js! Exclamation Points!!!!!!!!");
+				if(counter==0){
+					game.turn(1);
+					counter++;
+				}
+				else{
+					game.turn(2);
+					counter--;
+				}
 			}
-			else{
-				game.turn(2);
-				counter--;
-			}
+			System.out.println("Would you like to play again? (y/n)");
+			uIn = scan.nextLine();
+			if(uIn.equals("y")) break;
 		}
-		
 	}
-
 }
