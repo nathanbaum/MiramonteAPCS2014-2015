318e9ec762d14b1a096552a82bc05c7c9eb40264
Nathan Baum <nbaum510@gmail.com>
2015-02-22 17:59:36 -0800

    Lab Inheritance setup

--- Lab Inheritance/src/inheritancePackage/Hourly.java
+++ Lab Inheritance/src/inheritancePackage/Hourly.java
@@ -5,40 +5,39 @@ package inheritancePackage;
 //
 //Represents an employee that gets paid by the hour.
 //********************************************************************
-public class Hourly extends Employee
-{
+public class Hourly extends Employee {
 	private int hoursWorked;
-	//-----------------------------------------------------------------
-	//  Sets up this hourly employee using the specified information.
-	//-----------------------------------------------------------------
-	public Hourly (String eName, String eAddress, String ePhone,
-			String socSecNumber, double rate)
-	{
-		super (eName, eAddress, ePhone, socSecNumber, rate);
+
+	// -----------------------------------------------------------------
+	// Sets up this hourly employee using the specified information.
+	// -----------------------------------------------------------------
+	public Hourly(String eName, String eAddress, String ePhone,
+			String socSecNumber, double rate) {
+		super(eName, eAddress, ePhone, socSecNumber, rate);
 		hoursWorked = 0;
 	}
-	//-----------------------------------------------------------------
-	//  Adds the specified number of hours to this employee’s
-	//  accumulated hours.
-	//-----------------------------------------------------------------
-	public void addHours (int moreHours)
-	{
+
+	// -----------------------------------------------------------------
+	// Adds the specified number of hours to this employee’s
+	// accumulated hours.
+	// -----------------------------------------------------------------
+	public void addHours(int moreHours) {
 		hoursWorked += moreHours;
 	}
-	//-----------------------------------------------------------------
-	//  Computes and returns the pay for this hourly employee.
-	//-----------------------------------------------------------------
-	public double pay()
-	{
+
+	// -----------------------------------------------------------------
+	// Computes and returns the pay for this hourly employee.
+	// -----------------------------------------------------------------
+	public double pay() {
 		double payment = getPayRate() * hoursWorked;
 		hoursWorked = 0;
 		return payment;
 	}
-	//-----------------------------------------------------------------
-	//  Returns information about this hourly employee as a string.
-	//-----------------------------------------------------------------
-	public String toString()
-	{
+
+	// -----------------------------------------------------------------
+	// Returns information about this hourly employee as a string.
+	// -----------------------------------------------------------------
+	public String toString() {
 		String result = super.toString();
 		result += "\nCurrent hours: " + hoursWorked;
 		return result;
