bf7b8efc1549acd3ce7c30c9d7607d1a5e11ec08
3004728 <3004728@MM-444-13.mhs.acalanes.k12.ca.us>
2015-03-03 14:10:54 -0800

    corrections corrections

--- Lab Inheritance/src/dogsPackage/DogTest.java
+++ Lab Inheritance/src/dogsPackage/DogTest.java
@@ -6,14 +6,17 @@ public class DogTest {
 		Labrador lab = new Labrador("Rover", "brown");
 		Yorkshire york = new Yorkshire("Yorkie", "white");
 		
+		//prints Spike stuff
 		System.out.println(dog.getName() + " is a " + dog.getClass());
 		System.out.println(dog.getName() + " says " + dog.speak());
 		System.out.println();
 		
+		//prints Rover stuff
 		System.out.println(lab.getName() + " is a " + lab.getClass());
 		System.out.println(lab.getName() + " says " + lab.speak() + ", is " + lab.getColor() + ", and weighs " + lab.getBreedWeight() + ".");
 		System.out.println();
 		
+		//prints Yorkie stuff
 		System.out.println(york.getName() + " is a " + york.getClass());
 		System.out.println(york.getName() + " says " + york.speak() + ", is " + york.getColor() + ", and weighs " + york.getBreedWeight() + ".");
 		System.out.println();
