5e3f4e65a5794b7ca88b94c9c9cce50c9b972c55
3004728 <3004728@MM-444-13.mhs.acalanes.k12.ca.us>
2015-03-06 14:11:44 -0800

    Start of Inheritance, Abstract, Interface Lab

---
+++ Lab Inheritance/src/inheritanceAbstractionAndInterfacesPackage/Store.java
@@ -0,0 +1,21 @@
+package inheritanceAbstractionAndInterfacesPackage;
+
+public abstract class Store {
+	private int sqFootage, numDoors, numWindows;
+	
+	public Store(int sqFoot, int numDoors, int numWindows){
+		
+	}
+
+	public int getSqFootage() {
+		return sqFootage;
+	}
+
+	public int getNumDoors() {
+		return numDoors;
+	}
+
+	public int getNumWindows() {
+		return numWindows;
+	}
+}
