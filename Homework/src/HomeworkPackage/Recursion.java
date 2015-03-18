5dbd9930b305d846fbd685a75d58902a0dae6722
Nathan Baum <nbaum510@gmail.com>
2015-03-17 23:49:11 -0700

    work on recursion

--- Homework/src/HomeworkPackage/Recursion.java
+++ Homework/src/HomeworkPackage/Recursion.java
@@ -6,7 +6,44 @@ public class Recursion {
 		System.out.println(factorial(2));
 		System.out.println(mult(0, 7));
 		System.out.println(fibonacci(6));
+		System.out.println(strCopies("catcowcat", "cat", 2));
 
+	}
+	
+	public static boolean groupSum6(int start, int[] nums, int target){
+		if(nums.length>0){
+			if(nums[start]==6 && target>6){
+				return true && groupSum6(start+1, nums, target-6);
+			}
+			else{
+				if(target>nums[start]){
+					return true && groupSum6(start+1, nums, target-nums[start]);
+				}
+				else if(target==nums[start]){
+					return true;
+				}
+				else return false;
+			}
+		}
+		else if(target==0){
+			return true;
+		}
+		else return false;
+	}
+	
+	public static boolean strCopies(String str, String sub, int n) {
+		//System.out.println("str: " + str + "  sub: " + sub + "  n: " + n);
+		for(int i=0; i<str.length()-(sub.length()-1); i++){
+		  //System.out.print(" i:" + i);
+		  if(str.length()>=sub.length() && str.substring(i, i+sub.length()).equals(sub)){
+		    if(n>1){
+		      return true && strCopies(str.substring(i+1), sub, n-1);
+		    }
+		    else if(n==1) return true;
+		  }
+		}
+		if(n>0) return false;
+		else return true;
 	}
 	
 	public static int mult(int factor1, int factor2){
